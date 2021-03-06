709d37db74049d2e8284e4eda0886f005a764b0c
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2021-03-06 07:04:50 +0530

    fetched data from server

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Row, Col, ListGroup, Image, Card, Button } from 'react-bootstrap';
-import products from '../products';
-import Rating from '../components/Rating';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { Row, Col, ListGroup, Image, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
 
 const ProductScreen = ({ match }) => {
-    const product = products.find((p) => p._id === match.params.id);
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(
+                `/api/products/${match.params.id}`
+            );
+            setProduct(data);
+        };
+
+        fetchProduct();
+    }, [match]);
 
     return (
         <>
-            <Link className='btn btn-light' to='/'>
+            <Link className="btn btn-light" to="/">
                 Go Back
             </Link>
             <Row>
@@ -18,24 +29,29 @@ const ProductScreen = ({ match }) => {
                 </Col>
 
                 <Col md={3}>
-                    <ListGroup variant='flush'>
+                    <ListGroup variant="flush">
                         <ListGroup.Item>
                             <h3>{product.name}</h3>
                         </ListGroup.Item>
 
                         <ListGroup.Item>
-                            <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                            <Rating
+                                value={product.rating}
+                                text={`${product.numReviews} reviews`}
+                            />
                         </ListGroup.Item>
 
                         <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
 
-                        <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+                        <ListGroup.Item>
+                            Description: {product.description}
+                        </ListGroup.Item>
                     </ListGroup>
                 </Col>
 
                 <Col md={3}>
                     <Card>
-                        <ListGroup variant='flush'>
+                        <ListGroup variant="flush">
                             <ListGroup.Item>
                                 <Row>
                                     <Col>Price:</Col>
@@ -48,12 +64,20 @@ const ProductScreen = ({ match }) => {
                             <ListGroup.Item>
                                 <Row>
                                     <Col>Status:</Col>
-                                    <Col>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</Col>
+                                    <Col>
+                                        {product.countInStock > 0
+                                            ? "In Stock"
+                                            : "Out of Stock"}
+                                    </Col>
                                 </Row>
                             </ListGroup.Item>
 
                             <ListGroup.Item>
-                                <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
+                                <Button
+                                    className="btn-block"
+                                    type="button"
+                                    disabled={product.countInStock === 0}
+                                >
                                     Add to Cart
                                 </Button>
                             </ListGroup.Item>
