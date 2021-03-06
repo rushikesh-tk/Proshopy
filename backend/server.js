e62ad914a3756277d9a8739b1f9cf791b983a215
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2021-03-06 07:02:50 +0530

    server created

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,28 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
+
+dotenv.config();
 
 const app = express();
 
-app.listen("5000", console.log("Running on 5000"));
+app.get("/", (req, res) => {
+	res.send("Bsdk bete.....mauj kardi");
+});
+
+app.get("/api/products", (req, res) => {
+	res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+	const product = products.find((p) => p._id === req.params.id);
+
+	res.json(product);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+	PORT,
+	console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
